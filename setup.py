2ad30520adf678f03db8f4ae3dfbf6456317f6f6
Steffen Heyne <heyne@ie-freiburg.mpg.de>
2019-04-17 17:30:19 +0200

    test

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 from glob import glob
+from distutils.core import setup, Command
+import distutils.command.install 
 import os.path
 
 # Set __version__
@@ -9,6 +11,31 @@ exec(open('snakePipes/__init__.py').read())
 scripts = ['bin/snakePipes']
 for d in glob('snakePipes/workflows/*'):
     scripts.append(os.path.join(d, os.path.split(d)[1]))
+
+
+class InstallCommand(Command):
+    description = "Installs the foo."
+    user_options = [
+        ('mpi-organisms', None, 'Insall the organism files.'),
+    ]
+    def initialize_options(self):
+        self.foo = None
+    def finalize_options(self):
+        assert self.foo in (None, 'myFoo', 'myFoo2'), 'Invalid foo!'
+    def run(self):
+        print("test")
+
+class BuildPyCommand(distutils.command.install.install):
+    user_options = distutils.command.install.install.user_options + [
+        ('foo=', None, 'Insall the organism files.'),
+    ]
+    def initialize_options(self):
+        self.foo = None
+    def finalize_options(self):
+        assert self.foo in (None, 'myFoo', 'myFoo2'), 'Invalid foo!'
+    def run(self):
+        distutils.command.install.install.run(self)
+        print("test")
 
 setup(
     name='snakePipes',
@@ -20,4 +47,7 @@ setup(
     license='GPL v3',
     description='Snakemake workflows and wrappers for NGS data processing from the MPI-IE',
     zip_safe=False,
+    cmdclass={
+        'install': BuildPyCommand
+    }
 )
